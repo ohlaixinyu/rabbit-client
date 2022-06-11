a3ce59bfec0579f0a6e5d8218e76b465650b3a37
unknown <1365870192@qq.com>
2022-06-11 11:06:40 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      APP
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'APP',
+  setup () {
+  }
+}
+</script>
+
+<style scoped >
+
+</style>
---
+++ src/views/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+    vue3
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'layout-page',
+  setup () {
+  }
+}
+</script>
+
+<style scoped >
+
+</style>
